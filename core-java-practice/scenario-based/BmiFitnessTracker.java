fb9bd6b8f665394389e106cacf03b7bab9725b6f
SumitSaini1 <sumitsaini21027@gmail.com>
2025-12-24 19:20:51 +0530

    [Sumit Saini] Style: Improve BMI Report Output Formatting

--- core-java-practice/scenario-based/BmiFitnessTracker.java
+++ core-java-practice/scenario-based/BmiFitnessTracker.java
@@ -1,34 +1,44 @@
 import java.util.Scanner;
 
 public class BmiFitnessTracker {
-    public static void CalculateBmi(){
+    public static void CalculateBmi() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your weight in KG :");
 
-        double weight =input.nextDouble();
+        double weight = input.nextDouble();
         System.out.println("Enter your height in cm :");
 
-        double heightInCm=input.nextDouble(); // take height in centimeters
-        double heightInMeter= heightInCm / 100; // convert Cm to Meter 
+        double heightInCm = input.nextDouble(); // take height in centimeters
+        double heightInMeter = heightInCm / 100; // convert Cm to Meter
 
-        double BMI= weight / (heightInMeter * heightInMeter); // Calculate formula for BMI 
+        double BMI = weight / (heightInMeter * heightInMeter); // Calculate formula for BMI
 
-        if(BMI <= 18.4){
-            System.out.println("Person BMI " + BMI + ", Status: Underweight" );
-        }else if(BMI >= 18.5 && BMI <= 24.9){
-            System.out.println("Person BMI " + BMI + ", Status: Normal" );
+        System.out.println("--------- BMI REPORT ---------");
 
-        }else if(BMI >= 25.0 && BMI <= 39.9){
-            System.out.println("Person BMI " + BMI + ", Status: OverWeight" );
+        if (BMI <= 18.4) {
+            System.out.printf("BMI Value : %.2f%n", BMI);
+            System.out.println("Status    : Underweight");
 
-        }else{
-            System.out.println("Person BMI " + BMI + ", Status: obese" );
+        } else if (BMI <= 24.9) {
+            System.out.printf("BMI Value : %.2f%n", BMI);
+            System.out.println("Status    : Normal");
 
+        } else if (BMI <= 39.9) {
+            System.out.printf("BMI Value : %.2f%n", BMI);
+            System.out.println("Status    : Overweight");
+
+        } else {
+            System.out.printf("BMI Value : %.2f%n", BMI);
+            System.out.println("Status    : Obese");
         }
+
+        System.out.println("------------------------------");
         input.close();
+
         
 
     }
+
     public static void main(String[] args) {
         BmiFitnessTracker.CalculateBmi();
     }
